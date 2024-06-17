56200d1da6a323ccb09815b4f0b72688b692e914
hyperbo1a <152983476+hyperbo1a@users.noreply.github.com>
2024-06-17 17:15:44 +0300

    Update OpenWeatherMapController.cs

--- GalutinisProjektas.Server/Controllers/OpenWeatherMapController.cs
+++ GalutinisProjektas.Server/Controllers/OpenWeatherMapController.cs
@@ -9,11 +9,12 @@ using System.Text.Json;
 
 namespace GalutinisProjektas.Server.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
     ///<summary>
     /// Controller for retrieving air pollution data
     /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    
     public class OpenWeatherMapController : Controller
     {
         private const string RouteName = "air-pollution";
@@ -21,24 +22,29 @@ namespace GalutinisProjektas.Server.Controllers
         private readonly OpenWeatherMapService _openWeatherMapService;
         
         
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpenWeatherMapController"/> class.
+        /// </summary>
+        /// <param name="logger">Logger instance for logging.</param>
+        /// <param name="openWeatherMapService">Service instance for interacting with the OpenWeatherMap API.</param>
         public OpenWeatherMapController(ILogger<OpenWeatherMapController> logger,OpenWeatherMapService openWeatherMapService)
         {
             _openWeatherMapService = openWeatherMapService;
             _logger = logger;
         }
 
-         /// <summary>
-         /// Retrieves air pollution data based on coordinates 
-         /// </summary>
-         /// <param name="latitude">The latitude of the location</param>
-         /// <param name="longitude">The longitude of the location</param>
-         /// <returns> The air pollution data for the specified location</returns>
-         /// <remarks>
-         ///  Queries the OpenWeatherMap API for air pollution data based on the specified coordinates
-         ///  </remarks>
-         /// <response code="201">Returns the air pollution data for the specified location</response>
-         /// <response code="400">If the request is invalid</response>
+        /// <summary>
+        /// Retrieves air pollution data based on coordinates.
+        /// </summary>
+        /// <param name="latitude">The latitude of the location.</param>
+        /// <param name="longitude">The longitude of the location.</param>
+        /// <returns>The air pollution data for the specified location.</returns>
+        /// <remarks>
+        /// Queries the OpenWeatherMap API for air pollution data based on the specified coordinates.
+        /// </remarks>
+        /// <response code="200">Returns the air pollution data for the specified location.</response>
+        /// <response code="400">If the request is invalid.</response>
+        /// <response code="500">If an internal server error occurs.</response>
         [HttpPost(Name = RouteName)]
         [ProducesResponseType(typeof(AirPollutionResponse), 201)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
