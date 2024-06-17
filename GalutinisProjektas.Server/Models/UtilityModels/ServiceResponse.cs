78592df2806b3bac4615dbc8ef32642e84c9ea00
hyperbo1a <152983476+hyperbo1a@users.noreply.github.com>
2024-06-17 18:42:18 +0300

    Update ServiceResponse.cs

--- GalutinisProjektas.Server/Models/UtilityModels/ServiceResponse.cs
+++ GalutinisProjektas.Server/Models/UtilityModels/ServiceResponse.cs
@@ -1,9 +1,24 @@
 ﻿namespace GalutinisProjektas.Server.Models.UtilityModels
 {
+    /// <summary>
+    /// Represents a generic service response containing data of type T, status code, and optional error message.
+    /// </summary>
+    /// <typeparam name="T">The type of data contained in the response.</typeparam>
     public class ServiceResponse<T>
     {
+        /// <summary>
+        /// Gets or sets the data returned by the service operation.
+        /// </summary>
         public T? Data { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status code indicating the result of the service operation.
+        /// </summary>
         public int StatusCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the error message if the service operation encountered an error.
+        /// </summary>
         public string? ErrorMessage { get; set; }
     }
 }
