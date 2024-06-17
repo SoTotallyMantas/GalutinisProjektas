0e539b8a3fc41760983cedbc03b9298b7601dfcb
hyperbo1a <152983476+hyperbo1a@users.noreply.github.com>
2024-06-17 18:20:26 +0300

    Update FlightEstimateResponse.cs

--- GalutinisProjektas.Server/Models/FlightResponse/FlightEstimateResponse.cs
+++ GalutinisProjektas.Server/Models/FlightResponse/FlightEstimateResponse.cs
@@ -3,11 +3,20 @@ using GalutinisProjektas.Server.Models.UtilityModels;
 
 namespace GalutinisProjektas.Server.Models.FlightResponse
 {
+    /// <summary>
+    /// Represents the response structure for flight carbon emission estimates.
+    /// </summary>
     public class FlightEstimateResponse
     {
+        /// <summary>
+        /// Gets or sets the main data containing flight carbon emission estimates.
+        /// </summary>
         [JsonPropertyName("data")]
         public required FlightResponseData Data { get; set; }
 
+        /// <summary>
+        /// Gets or sets the collection of HATEOAS links associated with the response.
+        /// </summary>
         public List<HATEOASLink> Links { get; set; } = new List<HATEOASLink>();
 
     }
