7cfa8981d00fc81f013a4505dcd4f6f07aab3696
hyperbo1a <152983476+hyperbo1a@users.noreply.github.com>
2024-06-17 17:58:28 +0300

    Update WeatherData.cs

--- GalutinisProjektas.Server/Models/AirPollutionResponse/WeatherData.cs
+++ GalutinisProjektas.Server/Models/AirPollutionResponse/WeatherData.cs
@@ -2,13 +2,26 @@
 
 namespace GalutinisProjektas.Server.Models.AirPollutionResponse
 {
+    /// <summary>
+    /// Represents weather data associated with air pollution.
+    /// </summary>
     public class WeatherData
     {
+        /// <summary>
+        /// Gets or sets the timestamp of the weather data.
+        /// </summary>
         [JsonPropertyName("dt")]
         public long Dt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the main air pollution parameters.
+        /// </summary>
         [JsonPropertyName("main")]
         public AirPollutionMain Main { get; set; }
 
+        /// <summary>
+        /// Gets or sets the components of air pollution.
+        /// </summary>
         [JsonPropertyName("components")]
         public AirPollutionComponents Components { get; set; }
 
