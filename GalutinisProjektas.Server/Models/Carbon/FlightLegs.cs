3969c0c0b1b16b052f7d4d573e359073e577a002
hyperbo1a <152983476+hyperbo1a@users.noreply.github.com>
2024-06-17 18:07:49 +0300

    Update FlightLegs.cs

--- GalutinisProjektas.Server/Models/Carbon/FlightLegs.cs
+++ GalutinisProjektas.Server/Models/Carbon/FlightLegs.cs
@@ -3,11 +3,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace GalutinisProjektas.Server.Models.Carbon
 {
+    /// <summary>
+    /// Represents the departure and destination airports of a flight leg.
+    /// </summary>
     public class FlightLegs
     {
+        /// <summary>
+        /// Gets or sets the departure airport code.
+        /// </summary>
         [Required]
         [SwaggerSchema("The departure airport of the flight")]
         public required string departure_airport { get; set; }
+
+        /// <summary>
+        /// Gets or sets the destination airport code.
+        /// </summary>
         [Required]
         [SwaggerSchema("The destination airport of the flight")]
         public required string destination_airport { get; set; }
