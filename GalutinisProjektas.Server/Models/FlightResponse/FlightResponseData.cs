0f37856d7929b56050aae778dbe4bae856c53b4c
hyperbo1a <152983476+hyperbo1a@users.noreply.github.com>
2024-06-17 18:24:24 +0300

    Update FlightResponseData.cs

--- GalutinisProjektas.Server/Models/FlightResponse/FlightResponseData.cs
+++ GalutinisProjektas.Server/Models/FlightResponse/FlightResponseData.cs
@@ -2,12 +2,26 @@
 
 namespace GalutinisProjektas.Server.Models.FlightResponse
 {
+    /// <summary>
+    /// Represents the data structure of a flight estimate response.
+    /// </summary>
     public class FlightResponseData
     {
+        /// <summary>
+        /// Gets or sets the ID associated with the flight estimate data.
+        /// </summary>
         [JsonPropertyName("id")]
         public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the flight estimate response data.
+        /// </summary>
         [JsonPropertyName("type")]
         public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the attributes associated with the flight estimate response.
+        /// </summary>
         [JsonPropertyName("attributes")]
         public FlightResponseAttributes Attributes { get; set; }
 
