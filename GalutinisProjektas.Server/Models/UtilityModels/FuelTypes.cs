2ec936a56cc97de655ef7ca5388cc761c98aa521
hyperbo1a <152983476+hyperbo1a@users.noreply.github.com>
2024-06-17 18:38:45 +0300

    Update FuelTypes.cs

--- GalutinisProjektas.Server/Models/UtilityModels/FuelTypes.cs
+++ GalutinisProjektas.Server/Models/UtilityModels/FuelTypes.cs
@@ -3,19 +3,34 @@ using System.ComponentModel.DataAnnotations;
 
 namespace GalutinisProjektas.Server.Models.UtilityModels
 {
+    /// <summary>
+    /// Represents a model class for storing fuel types.
+    /// </summary>
     public class FuelTypes
     {
-        // CarbonInterface API fuel types Entity Frame Work model
+        /// <summary>
+        /// Gets or sets the unique identifier for the fuel type.
+        /// </summary>
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of the fuel.
+        /// </summary>
         [Required]
         public string FuelName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the fuel.
+        /// </summary>
         [Required]
         public string FuelType { get; set; }
-        [Required]
 
+        /// <summary>
+        /// Gets or sets the unit of measurement for the fuel.
+        /// </summary>
+        [Required]
         public string FuelUnit { get; set; }
 
     }
