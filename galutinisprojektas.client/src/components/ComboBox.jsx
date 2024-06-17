dbff5738648c1f26dca098cae8e125e5447f9cc6
Krybas <arnas.silaikis@gmail.com>
2024-06-17 17:00:04 +0300

    comboBox style and fix of browser theme

--- galutinisprojektas.client/src/components/ComboBox.jsx
+++ galutinisprojektas.client/src/components/ComboBox.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
+import "./ComboBox.css";
 
-const ComboBox = ({ label, options }) => {
+const ComboBox = ({ label, options, onSelect, disable }) => {
     const [selectedOption, setSelectedOption] = useState('');
 
     const handleChange = (event) => {
-        setSelectedOption(event.target.value);
+        const value = event.target.value;
+        setSelectedOption(value);
+        if (onSelect) {
+            onSelect(value);
+        }
     };
 
     return (
-        <div>
+        <div className="combo-box">
             <label>{label}</label>
             <select id="combo-box" value={selectedOption} onChange={handleChange}>
                 <option value="">Select an option</option>
