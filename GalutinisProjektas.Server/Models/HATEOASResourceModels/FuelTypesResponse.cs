34505d62af74e9fdf3c1479e0e56cf0f3f697bf4
hyperbo1a <152983476+hyperbo1a@users.noreply.github.com>
2024-06-17 18:33:49 +0300

    Update FuelTypesResponse.cs

--- GalutinisProjektas.Server/Models/HATEOASResourceModels/FuelTypesResponse.cs
+++ GalutinisProjektas.Server/Models/HATEOASResourceModels/FuelTypesResponse.cs
@@ -3,8 +3,14 @@ using System.Text.Json.Serialization;
 
 namespace GalutinisProjektas.Server.Models.HATEOASResourceModels
 {
+    /// <summary>
+    /// Represents a response model for fuel type information with HATEOAS links.
+    /// </summary>
     public class FuelTypesResponse : FuelTypes
     {
+        /// <summary>
+        /// Gets or sets the collection of HATEOAS links related to the fuel type.
+        /// </summary>
         [JsonPropertyOrder(5)]
         public List<HATEOASLink> Links { get; set; }
 
