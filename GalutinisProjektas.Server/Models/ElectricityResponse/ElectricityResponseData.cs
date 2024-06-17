80938e8118e1bdb405b34447d3c3dd27bf63a91b
hyperbo1a <152983476+hyperbo1a@users.noreply.github.com>
2024-06-17 18:19:33 +0300

    Update ElectricityResponseData.cs

--- GalutinisProjektas.Server/Models/ElectricityResponse/ElectricityResponseData.cs
+++ GalutinisProjektas.Server/Models/ElectricityResponse/ElectricityResponseData.cs
@@ -2,12 +2,26 @@
 
 namespace GalutinisProjektas.Server.Models.ElectricityResponse
 {
+    /// <summary>
+    /// Represents the data structure of an electricity consumption response.
+    /// </summary>
     public class ElectricityResponseData
     {
+        /// <summary>
+        /// Gets or sets the identifier of the electricity consumption data.
+        /// </summary>
         [JsonPropertyName("id")]
         public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the electricity consumption data.
+        /// </summary>
         [JsonPropertyName("type")]
         public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the attributes of the electricity consumption data.
+        /// </summary>
         [JsonPropertyName("attributes")]
         public ElectricityResponseAttributes Attributes { get; set; }
 
