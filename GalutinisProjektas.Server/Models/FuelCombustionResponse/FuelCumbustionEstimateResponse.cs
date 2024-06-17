970025a28745f93601823367ef55145d613bed32
hyperbo1a <152983476+hyperbo1a@users.noreply.github.com>
2024-06-17 18:28:56 +0300

    Update FuelCumbustionEstimateResponse.cs

--- GalutinisProjektas.Server/Models/FuelCombustionResponse/FuelCumbustionEstimateResponse.cs
+++ GalutinisProjektas.Server/Models/FuelCombustionResponse/FuelCumbustionEstimateResponse.cs
@@ -3,11 +3,20 @@ using GalutinisProjektas.Server.Models.UtilityModels;
 
 namespace GalutinisProjektas.Server.Models.FuelCombustionResponse
 {
+    /// <summary>
+    /// Represents the response structure for fuel combustion estimates.
+    /// </summary>
     public class FuelCumbustionEstimateResponse
     {
+        /// <summary>
+        /// Gets or sets the fuel combustion data.
+        /// </summary>
         [JsonPropertyName("data")]
         public required FuelCombustionResponseData Data { get; set; }
 
+        /// <summary>
+        /// Gets or sets the collection of HATEOAS links related to the fuel combustion response.
+        /// </summary>
         public List<HATEOASLink> Links { get; set; } = new List<HATEOASLink>();
     }
 }
