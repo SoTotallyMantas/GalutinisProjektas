c6b17a8e14db6e9d3ba8cfb7bc4fd16bb83823d7
hyperbo1a <152983476+hyperbo1a@users.noreply.github.com>
2024-06-17 18:03:32 +0300

    Update CarbonFlight.cs

--- GalutinisProjektas.Server/Models/Carbon/CarbonFlight.cs
+++ GalutinisProjektas.Server/Models/Carbon/CarbonFlight.cs
@@ -5,21 +5,37 @@ using System.ComponentModel.DataAnnotations;
 
 namespace GalutinisProjektas.Server.Models.Carbon
 {
+    /// <summary>
+    /// Represents flight-related parameters for carbon emissions calculation.
+    /// </summary>
     [SwaggerSchema(Required = new[] { "Description" })]
     public class CarbonFlight
     {
-        
+        /// <summary>
+        /// Gets or sets the type of flight.
+        /// This property is not bound or validated during model binding.
+        /// </summary>
         [BindNever]
         [ValidateNever]
-       
         public required string type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of passengers on the flight.
+        /// </summary>
         [Required]
         [SwaggerSchema("The number of passengers on the flight")]
         public required int passengers { get; set; }
+
+        /// <summary>
+        /// Gets or sets the flight legs array.
+        /// </summary>
         [Required]
         [SwaggerSchema("Flight Departure/Destination Array")]
         public required FlightLegs[] legs { get; set; }
 
+        /// <summary>
+        /// Gets or sets the distance unit of the flight.
+        /// </summary>
         [Required]
         [SwaggerSchema("The distance unit of the flight")]
         public required string distance_unit { get; set; }
